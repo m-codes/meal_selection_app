243073b4a0c20362a1dbadec574883eff9de281e
m-codes <martinwalshsd@gmail.com>
2020-03-30 12:41:42 +0100

    Added meal state handling to main file

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,52 @@
 import 'package:flutter/material.dart';
 
+import './data/dummy_data.dart';
 import './screens/filters_screen.dart';
 import './screens/tabs_screeen.dart';
 import './screens/category_meals_screen.dart';
 import './screens/categories_screen.dart';
 import './screens/meal_detail_screen.dart';
+import './models/meal.dart';
 
 void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  Map<String, bool> _filters = {
+    'gluten': false,
+    'lactose': false,
+    'vegan': false,
+    'vegeterian': false,
+  };
+
+  List<Meal> _avaialbleMeals = DUMMY_MEALS;
+
+  void _setFilters(Map<String, bool> filterData){
+    setState(() {
+      _filters = filterData;
+
+      _avaialbleMeals = DUMMY_MEALS.where((meal) {
+        if(_filters['gluten'] && !meal.isGlutenFree){
+          return false;
+        }
+        if(_filters['lactose'] && !meal.isLactoseFree){
+          return false;
+        }
+        if(_filters['vegan'] && !meal.isVegan){
+          return false;
+        }
+        if(_filters['vegeterian'] && !meal.isVegetarian){
+          return false;
+        }
+        return true;
+      }).toList();
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -36,9 +74,9 @@ class MyApp extends StatelessWidget {
       initialRoute: '/', // default is '/'
       routes: {
         '/': (ctx) => TabsScreen(), // Home
-        CategoryMealsScreen.routeName: (ctx) => CategoryMealsScreen(),
+        CategoryMealsScreen.routeName: (ctx) => CategoryMealsScreen(_avaialbleMeals),
         MealDetailScreen.routeName: (ctx) => MealDetailScreen(),
-        FiltersSceen.routeName: (ctx) => FiltersSceen(),
+        FiltersSceen.routeName: (ctx) => FiltersSceen(_filters, _setFilters),
       },
       //Last resort to try show something. Instead of the app crashing.
       onUnknownRoute: (settings) {
